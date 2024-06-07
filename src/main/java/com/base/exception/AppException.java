41c40cd54aa73522d7a547a69e3fb4777dc33187
huyhq1010 <huyhq101096@gmail.com>
2024-06-07 16:44:44 +0700

    upate project

--- src/main/java/com/base/exception/AppException.java
+++ src/main/java/com/base/exception/AppException.java
@@ -1,16 +1,22 @@
 package com.base.exception;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class AppException extends RuntimeException {
 
+    public AppException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
     private ErrorCode errorCode;
+
+    public ErrorCode getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+    }
+
+
 
 }
